166853da8f2ade3ec25667ae0eea96566b1a5f02
Thibaut Robert <thibaut.robert@worldline.com>
2018-10-16 17:49:32 +0200

    use blocking construct around kafka blocking calls
    this solves some timeout issues during concurrent recovery of multiples
    persistence units on slow machines

--- src/main/scala/akka/persistence/kafka/MetadataConsumer.scala
+++ src/main/scala/akka/persistence/kafka/MetadataConsumer.scala
@@ -4,10 +4,11 @@ import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.kafka.common.TopicPartition
 
 import scala.collection.JavaConverters._
+import scala.concurrent.blocking
 
 trait MetadataConsumer {
 
-  def nextOffsetFor(config: Map[String, Object], topic: String, partition: Int): Long = {
+  def nextOffsetFor(config: Map[String, Object], topic: String, partition: Int): Long = blocking {
     val tp       = new TopicPartition(topic, partition)
     val consumer = new KafkaConsumer[String, Array[Byte]](config.asJava)
     try {
